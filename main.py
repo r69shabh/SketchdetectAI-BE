f7af6540293c16838395902803911a8bc3a135de
Rishabh Gusain <rishabhgusain.6@gmail.com>
2024-10-01 18:18:49 +0530

    changed allowed origin to allowed origin

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ allowed_origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=['*', allowed_origins],
+    allow_origins=allowed_origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
